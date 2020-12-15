2843081dd15378114ba4ce65a2c772a52a20f9a3
Dimitar Chakarov <dimitar@dchakarov.com>
2020-12-15 18:00:33 +0000

    Add the main code plus some tests

--- Sources/RegexHelper/RegexHelper.swift
+++ Sources/RegexHelper/RegexHelper.swift
@@ -1,3 +1,27 @@
+import Foundation
+
 struct RegexHelper {
-    var text = "Hello, World!"
+    let pattern: String
+    
+    func parse(_ inputString: String) -> [String] {
+        var result = [String]()
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        let nsrange = NSRange(inputString.startIndex..<inputString.endIndex, in: inputString)
+        regex.enumerateMatches(in: inputString,
+                               options: [],
+                               range: nsrange) { (match, _, stop) in
+            guard let match = match else { return }
+            if match.numberOfRanges > 1 {
+                for i in 1..<match.numberOfRanges {
+                    if let captureRange = Range(match.range(at: i), in: inputString) {
+                        let str = String(inputString[captureRange])
+                        result.append(str)
+                    }
+                }
+                stop.pointee = true
+            }
+        }
+        
+        return result
+    }
 }
--- Tests/RegexHelperTests/RegexHelperTests.swift
+++ Tests/RegexHelperTests/RegexHelperTests.swift
@@ -2,14 +2,26 @@ import XCTest
 @testable import RegexHelper
 
 final class RegexHelperTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RegexHelper().text, "Hello, World!")
+    func testExtractTwoGroupsOfNumbers() {
+        let helper = RegexHelper(pattern: #"\[(\d+)\]\D+(\d+)"#)
+        let testString = "mem[56248] = 21501"
+        let testResult = helper.parse(testString)
+        XCTAssertEqual(testResult.first, "56248")
+        XCTAssertEqual(testResult.last, "21501")
+    }
+    
+    func testRangeAndLetters() {
+        let helper = RegexHelper(pattern: #"(\d+)-(\d+) (\w): (\w+)"#)
+        let testString = "1-13 g: gggfgggggggggggggz"
+        let testResult = helper.parse(testString)
+        XCTAssertEqual(testResult[0], "1")
+        XCTAssertEqual(testResult[1], "13")
+        XCTAssertEqual(testResult[2], "g")
+        XCTAssertEqual(testResult[3], "gggfgggggggggggggz")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExtractTwoGroupsOfNumbers", testExtractTwoGroupsOfNumbers),
+        ("testRangeAndLetters", testRangeAndLetters),
     ]
 }
